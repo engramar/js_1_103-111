584ba7f7e33184faef15ad5ce90187b380c894e7
Engramar Bollas <engramar.bollas@gmail.com>
2020-11-20 22:40:35 +1100

    Create js_1_103-111.js

---
+++ js_1_103-111.js
@@ -0,0 +1,77 @@
+#Generate Random Fractions with JavaScript
+function randomFraction() {
+
+  // Only change code below this line
+
+  return Math.random();
+
+  // Only change code above this line
+}
+
+#Generate Random Whole Numbers with JavaScript
+function randomWholeNum() {
+
+  // Only change code below this line
+  return Math.floor(Math.random() * 10);
+
+}
+
+#Generate Random Whole Numbers within a Range
+function randomRange(myMin, myMax) {
+  // Only change code below this line
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+  // Only change code above this line
+}
+
+#Use the parseInt Function
+function convertToInteger(str) {
+    return parseInt(str);
+}
+convertToInteger("56");
+
+#Use the parseInt Function with a Radix
+function convertToInteger(str) {
+    return parseInt(str, 2);
+}
+convertToInteger("10011");
+
+#Use the Conditional (Ternary) Operator
+function checkEqual(a, b) {
+  return a === b ? "Equal" : "Not Equal";
+}
+
+checkEqual(1, 2);
+
+#Use Multiple Conditional (Ternary) Operators
+function checkSign(num) {
+  return (num > 0) ? "positive" 
+    : (num < 0) ? "negative" 
+    : "zero";
+}
+
+checkSign(10);
+
+#Use Recursion to Create a Countdown
+// Only change code below this line
+function countdown(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const arr = countdown(n - 1);
+    arr.unshift(n);
+    return arr;
+  }
+}
+// Only change code above this line
+
+
+#Use Recursion to Create a Range of Numbers
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum - startNum === 0) {
+    return [startNum];
+  } else {
+    var numbers = rangeOfNumbers(startNum, endNum - 1);
+    numbers.push(endNum);
+    return numbers;
+  }
+}
